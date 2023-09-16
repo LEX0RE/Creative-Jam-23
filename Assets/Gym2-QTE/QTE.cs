229df82039fdb7d59467dfc5d787c4c7a2d5e229
Alexandre Gélinas <gelinas.alexandre.2001@gmail.com>
2023-09-16 15:40:26 -0400

    Update TextMesh

--- Assets/Gym2-QTE/QTE.cs
+++ Assets/Gym2-QTE/QTE.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class QTE : Singleton<QTE>
 {
+    [SerializeField] public int maxSequence = 5;
+    [SerializeField] public int maxTime = 10;
+    [SerializeField] public int timeBeforeCanvas = 1;
+    public GameObject canvas;
+    private bool eventStarted = false;
+    private bool result = false;
     private List<Player> players = new List<Player>();
-    // Start is called before the first frame update
+    public void Start()
+    {
+        canvas.SetActive(false);
+    }
 
     public void AddPlayer(Player p)
     {
         players.Add(p);
     }
 
-    private void StartQTEEvent(Player p)
+    public void StartQTEEvent(Player p)
     {
+        if (!eventStarted)
+        {
+            result = false;
+            eventStarted = true;
+            foreach (PlayerController player in players)
+            {
+                player.SetQTE(true);
+            }
+            MainCamera.Instance.MoveToPlayer(p);
+            StartCoroutine(Timer());
+        }
+    }
 
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(timeBeforeCanvas);
+        canvas.SetActive(true);
+        yield return new WaitForSeconds(maxTime);
+        foreach (PlayerController player in players)
+        {
+            player.SetQTE(true);
+        }
+    }
+
+    public void Reset()
+    {
+        players.Clear();
     }
 }
