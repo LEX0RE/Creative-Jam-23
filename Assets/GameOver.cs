e3519a1b2df880935e15cb9f7c9660818beb8409
NAD - GameJam <gamejam@LAB>
2023-09-16 20:14:23 -0400

    Interface menus, presque finie. Manque l'integration avec le reste

---
+++ Assets/GameOver.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class GameOver : MonoBehaviour
+{
+    public TMP_Text pointsText;
+    public void Setup(int winner)
+    {
+        gameObject.SetActive(true);
+        pointsText.text = "Winner : Player " + winner.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
+
