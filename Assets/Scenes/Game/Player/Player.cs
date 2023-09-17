e414f1c8281dda296a549a49860054805804c31a
Alexandre Gélinas <gelinas.alexandre.2001@gmail.com>
2023-09-17 08:45:11 -0400

    Update QTE

--- Assets/Scenes/Game/Player/Player.cs
+++ Assets/Scenes/Game/Player/Player.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public bool damagemoment = false;
-    lifeManager lifeMang;
+    lifeManager lifeManag;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Timer());
-        lifeMang =  GameObject.FindWithTag("GestionnaireVie").GetComponent<lifeManager>();
+        lifeManag = GameObject.FindWithTag("GestionnaireVie").GetComponent<lifeManager>();
     }
 
     // Update is called once per frame
@@ -18,27 +17,33 @@ public class Player : MonoBehaviour
         checkPlayerlife();
     }
 
-    IEnumerator Timer()
-    {
-        yield return new WaitForSeconds(5);
-        QTE.Instance.StartQTEEvent(this);
-    }
     public void eventFinishfight()
     {
         gameObject.GetComponentInParent<Animator>().SetBool("Attack", false);
         gameObject.GetComponentInParent<PlayerController>().OnAttack = false;
         damagemoment = false;
     }
-    public void StartHIT() {
+    public void StartHIT()
+    {
         damagemoment = true;
     }
-    public void checkPlayerlife() {
-        if (gameObject.GetComponentInParent<PlayerController>().gameObject.name == "Player1") {
-            if (lifeMang.player1life<=0) { gameObject.GetComponent<Animator>().SetBool("Death",true); }
+    public void checkPlayerlife()
+    {
+        if (gameObject.GetComponentInParent<PlayerController>().gameObject.name == "Player1")
+        {
+            if (lifeManag.player1life <= 0)
+            {
+                gameObject.GetComponent<Animator>().SetBool("Death", true);
+                QTE.Instance.StartQTEEvent(this);
+            }
         }
         if (gameObject.GetComponentInParent<PlayerController>().gameObject.name == "Player2")
         {
-            if (lifeMang.player2life <= 0) { gameObject.GetComponent<Animator>().SetBool("Death", true); }
+            if (lifeManag.player2life <= 0)
+            {
+                gameObject.GetComponent<Animator>().SetBool("Death", true);
+                QTE.Instance.StartQTEEvent(this);
+            }
         }
 
     }
